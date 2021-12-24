54ac9efcb6a5dc9575330ad9906b3295e3eb6a3d
Shakir Shakeel <shakir@SCI-LT-189.local>
2021-12-24 21:44:21 +0530

    changes for today

--- Shakir/session2.js
+++ Shakir/session2.js
@@ -1,10 +1,77 @@
-const print = (data)=>{
-    console.log(data)
+const fun1 = () => {
+  console.log("This is fun1");
+};
+
+const fun2 = (callback) => {
+  console.log("This is fun2");
+  callback();
+};
+
+// fun2(fun1)
+
+// const p1 = new Promise((res, rej) => {
+//   res("HelloWorld");
+// });
+
+// const p2 = new Promise((res, rej) => {
+//   res("HelloWorld222");
+// });
+
+// p1.then((e) => {
+//   console.log(e);
+//   p2.then((e) => {
+//     console.log(e);
+//   });
+// });
+
+const users = ["john", "vaibhav", "shakir", "aravind"];
+const userDetails = {
+  john: {
+    address: "usa",
+    phone: 999,
+  },
+  vaibhav: {
+    address: "india",
+    phone: 123,
+  },
+};
+
+const getUsers = () => {
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      res(users[0]);
+    }, 2000);
+  });
+};
+
+const getDetails = (user) => {
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      res(userDetails[user]);
+    }, 2000);
+  });
+};
+
+// getUsers().then((user) => {
+//   console.log(user);
+//   getDetails(user).then((details) => {
+//     console.log(details);
+//   });
+// });
+// console.log("Finished");
+
+// async/ await
+
+const getApiData = async ()=>{
+    const user = await getUsers();
+    const userDetails = await getDetails(user)
+    console.log(user)
+    console.log(userDetails)
 }
 
-const getData = (callback)=>{
-    const data = "fghjk"
-    print(data)
-}
+// getApiData();
 
-getData(print)
+const currentDate = new Date();
+console.log(currentDate)
+
+
