592a320881b95243068a4af569a476ca5ee7d3d3
Shakir Shakeel <shakir@SCI-LT-189.local>
2021-12-28 15:40:38 +0530

    todays work 28/12

---
+++ Shakir/modules/calc.js
@@ -0,0 +1,14 @@
+import square, {addNum, subtractNum, multiplyNum, name} from './util.js'
+
+// const addNum = (x,y)=>{
+//     const sum = x+y
+//     return sum
+// }
+
+
+console.log(addNum(5,4))
+console.log(subtractNum(5,4))
+console.log(multiplyNum(5,4))
+console.log(name)
+
+console.log(square(9))
---
+++ Shakir/modules/util.js
@@ -0,0 +1,21 @@
+export const addNum = (x,y)=>{
+    const sum = x+y
+    return sum
+}
+
+export const subtractNum = (x,y)=>{
+    const sum = x-y
+    return sum
+}
+
+export const multiplyNum = (x,y)=>{
+    const sum = x*y
+    return sum
+}
+
+export const name = "Hello World"
+
+const square = (n)=>{
+    return n*n
+}
+export default square;
